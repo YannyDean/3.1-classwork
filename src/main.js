9b0c101a6f78032529e0a48ff8d661a6642f7ef9
Yanny <larryanrenell@gmail.com>
2022-04-04 19:35:54 -0500

    updated assignment

--- src/main.js
+++ src/main.js
@@ -7,3 +7,10 @@ console.log (x * y);
 console.log (x / y);
 console.log (x % y);
 console.log (x ** y);
+
+console.log (x > y);
+console.log (x >= y);
+console.log (x < y);
+console.log (x <= y);
+console.log (x == y);
+console.log (x != y);
